c9cba8b47a1025be0ac3947a1c1ca47fe2551cb9
Salvatore Nalbone <salvatorenalbone.web@gmail.com>
2022-10-10 09:47:23 +0200

    Update startrelax.sh

--- streamdeck/startrelax.sh
+++ streamdeck/startrelax.sh
@@ -3,8 +3,9 @@ pkill -f firefox-developer-edition;
 pkill -f spotify;
 qdbus org.kde.ActivityManager /ActivityManager/Activities SetCurrentActivity $( grep -Po '.*?(?=\=relax)' ~/.config/kactivitymanagerdrc | awk "NR==1") &
 sleep 1
+xdotool mousemove 0 y;
 firefox-developer-edition &
-sleep 1
-xdotool mousemove 1930 30;
+sleep 2
+xdotool mousemove 1930 y;
 spotify
 
